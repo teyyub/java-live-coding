cbfe7633e34d64dc8a3194fd886fba6254a5f569
teyyub <teyyub392@gmail.com>
2022-02-14 17:26:04 +0400

    sum without loop

--- src/main/java/com/live/coding/recursive/sums/SumWithoutLoop.java
+++ src/main/java/com/live/coding/recursive/sums/SumWithoutLoop.java
@@ -2,7 +2,8 @@ package com.live.coding.recursive.sums;
 
 public class SumWithoutLoop {
     public static void main(String[] args) {
-        System.out.println(sum(100));
+        System.out.println(sum(10q
+                0));
     }
 
     static int sum(int num){
