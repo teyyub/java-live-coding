6c1f457ee8677badf31128b455cfcf0eddaa8d94
teyyub <teyyub392@gmail.com>
2022-02-14 17:23:47 +0400

    recursive sum

---
+++ src/main/java/com/live/coding/recursive/sums/SumWithoutLoop.java
@@ -0,0 +1,12 @@
+package com.live.coding.recursive.sums;
+
+public class SumWithoutLoop {
+    public static void main(String[] args) {
+        System.out.println(sum(100));
+    }
+
+    static int sum(int num){
+        if (num==0) return 0;
+        return num+sum(num-1);
+    }
+}
