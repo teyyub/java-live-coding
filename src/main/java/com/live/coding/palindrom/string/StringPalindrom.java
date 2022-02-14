33426b21a7dcb95a922470677d1f8cb7df1e37ef
teyyub <teyyub392@gmail.com>
2022-02-14 16:12:11 +0400

    string palindrom

---
+++ src/main/java/com/live/coding/palindrom/string/StringPalindrom.java
@@ -0,0 +1,16 @@
+package com.live.coding.palindrom.string;
+/**
+ * String Palindrome abba
+ * */
+public class StringPalindrom {
+    public static void main(String[] args) {
+        String source = "abba";
+        String target="";
+
+        for(int i=source.toCharArray().length-1;i>0;i--){
+            target  =target + source.charAt(i);
+        }
+        System.out.println(target.equals(target));
+    }
+
+}
