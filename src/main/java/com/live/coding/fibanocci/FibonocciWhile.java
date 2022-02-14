5ff4d4f2fa3fce977c72f27226a699fe0c412b37
teyyub <teyyub392@gmail.com>
2022-02-14 15:25:41 +0400

    fibonocci while and foor

--- src/main/java/com/live/coding/fibanocci/FibonocciWhile.java
+++ src/main/java/com/live/coding/fibanocci/FibonocciWhile.java
@@ -5,17 +5,17 @@ package com.live.coding.fibanocci;
  * 1 1 2 3 5 8 13
  * */
 
-public class Fibonocci {
+public class FibonocciWhile {
 
     public static void main(String[] args) {
 
-       int a = 0, b =1 ; //1
-        for(int i=0;i<10;i++){
+       int a = 0, b =1 , value=10,iterate=1; //1
+        while(iterate<=value){
             System.out.print(a + ", ");
             int c = a + b; // 1
             a=b; //2
             b = c; //5
-
+            iterate++;
 //            System.out.println(a +" " +b+ " " + c);
         }
     }
