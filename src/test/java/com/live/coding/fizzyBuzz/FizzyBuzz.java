42f9883c1e9b4a58316a565af30b45fdc337ab7c
teyyub <teyyub392@gmail.com>
2022-02-14 20:14:26 +0400

    fizzbuzz implemented

---
+++ src/test/java/com/live/coding/fizzyBuzz/FizzyBuzz.java
@@ -0,0 +1,37 @@
+package com.live.coding.fizzyBuzz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Say Fizz if the number is divisible by 3.
+ * Say Buzz if the number is divisible by 5.
+ * Say FizzBuzz if the number is divisible by both 3 and 5.
+ * Return the number itself, if the number is not divisible by 3 and 5.
+ *
+ * */
+public class FizzyBuzz {
+    static List<String> fizzBuzz = new ArrayList<String>();
+    public static void main(String[] args) {
+        checkNumber(20);
+        System.out.println(fizzBuzz);
+    }
+
+    private static void checkNumber(int number) {
+        for (int i = 1 ; i<=number ; i++){
+            if(i % 15 == 0){
+              fizzBuzz.add("FizzBuzz");
+            }
+            else if(i % 3 == 0){
+                fizzBuzz.add("Fizz");
+            }
+            else if(i % 5 == 0) {
+                fizzBuzz.add("Buzz");
+            }  else {
+                fizzBuzz.add(String.valueOf(i));
+            }
+
+        }
+
+    }
+}
