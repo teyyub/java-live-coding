923f52a040155fb39009fe7b5fa2b82c70c527c9
teyyub <teyyub392@gmail.com>
2022-02-14 20:26:05 +0400

    fizzbuzz implemeted for reading link

--- src/test/java/com/live/coding/fizzyBuzz/FizzyBuzz.java
+++ src/test/java/com/live/coding/fizzyBuzz/FizzyBuzz.java
@@ -8,7 +8,11 @@ import java.util.List;
  * Say Buzz if the number is divisible by 5.
  * Say FizzBuzz if the number is divisible by both 3 and 5.
  * Return the number itself, if the number is not divisible by 3 and 5.
- *
+ * */
+
+/**
+ * https://blog.eviltester.com/2018/03/acceptance-testing-fizzbuzz-java-junit.html
+ * https://blog.eviltester.com/2018/03/acceptance-testing-fizzbuzz-java-junit.html
  * */
 public class FizzyBuzz {
     static List<String> fizzBuzz = new ArrayList<String>();
