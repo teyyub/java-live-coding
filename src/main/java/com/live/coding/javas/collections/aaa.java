ee23b906722f3b9d671b6e10a2a53cd3f8e102a2
teyyub <teyyub392@gmail.com>
2022-02-22 21:52:21 +0400

    array readme

---
+++ src/main/java/com/live/coding/javas/collections/aaa.java
@@ -0,0 +1,28 @@
+package com.live.coding.javas.collections;
+
+public class aaa {
+    public static void main(String[] args) {
+        double a=0.1d;
+        System.out.println(a);
+        double c = a+a+a;
+        System.out.println(c);
+
+        System.out.println(a+a);
+
+        Double x1 = new Double("0.1");
+        Double c1 = x1.doubleValue()+x1.doubleValue()+x1.doubleValue();
+        System.out.println(c1);
+
+
+        System.out.println(Math.round(0.49999999999999994));
+
+        System.out.println(0.1d+0.2d);
+        System.out.println(0.3+0.6);
+
+        System.out.println(1/3*3 ==0.9);
+        System.out.println(1/3 ==0.3);
+        double vv =1/3;
+        System.out.println(vv);
+
+    }
+}
