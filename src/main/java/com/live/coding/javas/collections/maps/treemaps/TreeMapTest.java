9be1e545afd806dc85bac3fcc16927639b83135f
teyyub <teyyub392@gmail.com>
2022-02-25 21:37:22 +0400

    clone linked array lists

--- src/main/java/com/live/coding/javas/collections/maps/treemaps/TreeMapTest.java
+++ src/main/java/com/live/coding/javas/collections/maps/treemaps/TreeMapTest.java
@@ -2,7 +2,12 @@ package com.live.coding.javas.collections.maps.treemaps;
 
 import java.util.Map;
 import java.util.TreeMap;
-
+/**
+ * https://www.dineshonjava.com/internal-working-of-treemap-in-java/
+ * https://medium.com/xebia-engineering/treemap-internals-199e0e0050b5
+ * https://javahungry.blogspot.com/2014/06/how-treemap-works-ten-treemap-java-interview-questions.html
+ * https://www.java67.com/2012/08/difference-between-treemap-and-treeset-java.html
+ * */
 public class TreeMapTest {
     public static void main(String[] args) {
         Map<Integer,String> map=new TreeMap();
